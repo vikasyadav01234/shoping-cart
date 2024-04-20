a8e16bef6e30a88ccca9b14708bf274f96ba3b03
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-20 10:51:08 +0530

    main

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -2,9 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import CounterSlice from "./Slices/CounterSlice";
 
 export const store = configureStore({
-    reducer:{
         reducer : {
             counter: CounterSlice
-        }
-    },
+        },
 })
