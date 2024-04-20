3714efce7d77c42719bd9cfd33b8100caa74c72c
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-20 10:54:59 +0530

    main

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>
 );
