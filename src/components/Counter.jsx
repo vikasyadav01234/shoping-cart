b9a6c682f97d4f4ff42e624e771ba283615a5876
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-20 11:17:35 +0530

    main

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Counter = () => {
     const count = useSelector((state) => state.counter.value);
-
+    const dispatch = useDispatch();
 
   return (
     <div>
-        <button>
+        <button
+        
+        >
             Increment
         </button>
         <br/>
