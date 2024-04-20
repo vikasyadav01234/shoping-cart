1995c0146af00eeb86d4f6ad6e855d9860498a76
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-20 10:46:25 +0530

    main

--- src/redux/Slices/CounterSlice.jsx
+++ src/redux/Slices/CounterSlice.jsx
@@ -19,4 +19,4 @@ export const CounterSlice = createSlice({
 
 export const {increment, decrement} = CounterSlice.actions;
 
-export default CounterSlice.reducer
+export default CounterSlice.reducer;
