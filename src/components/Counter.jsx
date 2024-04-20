df5a893a2918eeaceb7fcc3704c1d5bae069d9eb
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-20 11:15:05 +0530

    mian

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 const Counter = () => {
-    const count = useSelector((state) => state.counter)
+    const count = useSelector((state) => state.counter.value);
 
 
   return (
